eb0561c3bba915584dde9245bd80ed27ea4a8d34
bnguien <nguyennguyendangbao8@gmail.com>
2025-10-08 09:30:09 +0700

    Update PacketType.cs

--- PBL4_BKDN/Common/Enums/PacketType.cs
+++ PBL4_BKDN/Common/Enums/PacketType.cs
@@ -11,14 +11,16 @@ namespace Common.Enums
         RemoteShellResponse = 13,
         FileManagerRequest = 14,
         FileManagerResponse = 15,
-
+        MessageBoxRequest = 16,
+        MessageBoxResponse = 17,
         // KeyLogger packets
         KeyLoggerStart = 100,
         KeyLoggerStop = 101,
         KeyLoggerEvent = 102, // single key event (parallel)
         KeyLoggerBatch = 103, // batch text (continuous)
         KeyLoggerComboEvent = 104, // detected key combination event
-        KeyLoggerLangToggle = 105
+        KeyLoggerLangToggle = 105,
+        
 
     }
 }

82b9b05a5219c860ca90352cb208fe72895210b7
bnguien <nguyennguyendangbao8@gmail.com>
2025-10-09 17:07:17 +0700

    Create ShutdownActionHandler.cs

---
+++ PBL4_BKDN/Server/Handlers/ShutdownActionHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Networking;
+
+namespace Server.Handlers
+{
+    public sealed class ShutdownActionHandler
+    {
+
+        private readonly ConcurrentDictionary<string, ShutdownActionResponse> _lastResponses = new ConcurrentDictionary<string, ShutdownActionResponse>();
+
+        public event EventHandler<ShutdownActionResponse>? OnResponseReceived;
+
+        public void SaveLastResponses(string clientId, ShutdownActionResponse response)
+        {
+            _lastResponses[clientId] = response;
+            OnResponseReceived?.Invoke(this, response);
+        }
+
+        public bool TryGetLastResponse(string clientId, out ShutdownActionResponse? response)
+        {
+            return _lastResponses.TryGetValue(clientId, out response);
+        }
+    }
+}

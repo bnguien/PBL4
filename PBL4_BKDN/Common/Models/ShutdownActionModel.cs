6f9835cfdb30c5379df66c80011a07740e7e65fe
bnguien <nguyennguyendangbao8@gmail.com>
2025-10-09 17:06:36 +0700

    Create ShutdownActionModel.cs

---
+++ PBL4_BKDN/Common/Models/ShutdownActionModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Enums;
+
+namespace Common.Models
+{
+    public sealed class ShutdownActionModel
+    {
+        public string ClientId { get; set; } = string.Empty;
+        public string ClientName { get; set; } = string.Empty;
+        public DateTime Timestamp { get; set; }
+        public ShutdownAction Action { get; set; }   
+        public CommandResultModel? CommandResult { get; set; }
+    }
+
+}
